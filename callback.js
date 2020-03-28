5558e1a3de4a72c8663b29a1439b719169c51854
Nazmul Hoque <coder.nazmul@gmail.com>
2020-03-28 20:55:51 +0600

    more knows about callback function

--- callback.js
+++ callback.js
@@ -1,14 +1,29 @@
 // It's called callback function
-
-
 function setName(name) {
     return name;
 }
 function greeting(msg, callback){
     return msg+", "+callback;
 }
-
 // If we pass a function as a argument of another function, is called callback function.
 const welcome = greeting('Welcome', setName("Nazmul Hoque"))
-
 console.log(welcome);
+
+// Have a fun with callback function 
+// We can use a function in multiple ways by using callback function 
+function sayGreeting (name, print){
+    print(name);
+}
+
+sayGreeting("Nazmul Hoque", function(data){
+    console.log("Hello, " + data);
+})
+
+sayGreeting("Nazmul Hoque", function(data){
+    console.log("Hey, How are you" + data + "?");
+})
+
+sayGreeting("Nazmul Hoque", function(data){
+    console.log("The Length of " + data + " is : " + data.length);
+})
+
